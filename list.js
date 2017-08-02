9433bdd6a1f1a47bac81f416a9ea707869827f42
Aicun <luaicun@gmail.com>
2017-08-01 23:08:30 -0400

    employee list attr append

---
+++ list.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $("#accordion").on("click","h2",function(){
+        $(this).toggleClass("minus");
+        if($(this).attr("class")!= "minus") {
+            $(this).next().hide();
+        }else {
+            $(this).next().show();
+        }
+    });
+
+    $("#add").click(function(){
+        var html = "";
+        html += "<h2><a href='#'>" + $("#name").val() + "</a></h2>";
+        html += "<div><h3>" + $("#position").val() + "</h3>";
+        html += "<p>" + $("#description").val() + "</p></div>";
+
+        $("#accordion").append(html);
+        $("#name").val("");
+    	$("#position").val("");
+    	$("#description").val("");
+    });
+
+});
